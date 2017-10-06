f786d93d9c8404bad36a905992d8792860efcae6
vladyakimow <31766832+vladyakimow@users.noreply.github.com>
2017-10-06 13:56:29 +0300

    Add files via upload

---
+++ lesson22.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+const int ft=12;
+int rost;
+cout << "vash rost v duymah:_____\b\b\b\b\b";
+cin >>rost;
+cout << "vash rost: " <<rost / ft << " futov i " << rost % ft << " duymov" << endl;
+return 0;
+}
