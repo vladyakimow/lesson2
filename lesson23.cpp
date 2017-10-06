ca58088df0fdd9ff136a3064b1845a05b9a9c5ce
vladyakimow <31766832+vladyakimow@users.noreply.github.com>
2017-10-06 14:02:23 +0300

    Add files via upload

---
+++ lesson23.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+int mile;
+int benzin;
+cout << "vi proehali:_____\b\b\b\b\b";
+cin >>mile;
+cout << "vi potratili benzina: ";
+cin >>benzin;
+ cout <<"mil' na 1 gallon:" <<mile/benzin << endl;
+return 0;
+}
